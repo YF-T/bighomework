b9d02b0ec11e4d2b875e018d669f6dfbdf8f231a
dnimpeed@163.com <dnimpeed@163.com>
2023-06-01 10:01:38 +0800

    Update: 查看其他用户个人主页，关注与被关注的数量还没有动态更新

--- app/app/src/main/java/com/example/myapplication/PersonalHomepageActivity.java
+++ app/app/src/main/java/com/example/myapplication/PersonalHomepageActivity.java
@@ -5,18 +5,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.function.Function;
 
 public class PersonalHomepageActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView usernameTextView;
+    private TextView descriptionTextView;
     private TextView followingTextView;
     private TextView followerTextView;
+    // 关注数需要动态更新
     private RecyclerView recyclerView;
     private DongTaiAdapter dongTaiAdapter;
 
@@ -27,6 +37,7 @@ public class PersonalHomepageActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageView);
         usernameTextView = findViewById(R.id.username);
+        descriptionTextView = findViewById(R.id.description);
         followingTextView = findViewById(R.id.following);
         followerTextView = findViewById(R.id.follower);
         recyclerView = findViewById(R.id.recyclerview);
@@ -41,6 +52,50 @@ public class PersonalHomepageActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         dongTaiAdapter = new DongTaiAdapter(this, getDongTaiData());
         recyclerView.setAdapter(dongTaiAdapter);
+
+        try {
+            WebRequest.sendGetRequest("/user/foreigninfo", new HashMap<>(), new Function<HashMap<String, Object>, Void>(){
+                @Override
+                public Void apply(HashMap<String, Object> response) {
+                    if (response != null && (boolean) response.get("status")) {
+                        Log.d("info", response.get("info").toString());
+                        HashMap<String, Object> info = null;
+                        try {
+                            info = JsonUtil.jsonObjectToHashMap((JSONObject)response.get("info"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        // Extract the user information from the response
+                        String username = (String) info.get("name");
+                        String description = (String) info.get("description");
+                        String image = (String) info.get("image");
+                        String following = (String) info.get("following");
+                        String follower = (String) info.get("follower");
+
+                        // Update the UI with the retrieved user information
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                usernameTextView.setText(username);
+                                descriptionTextView.setText(description);
+                                followingTextView.setText(following);
+                                followerTextView.setText(follower);
+                                WebRequest.downloadImage(GlobalVariable.get("userimageurl", "/image/user/abc.jpg"), bitmap -> {
+                                    // 在这里处理下载完成后的逻辑，例如将图片显示在ImageView中
+                                    // 是抄的UserInformationActivity，这个代码很像只能返回默认头像的样子……
+                                    imageView.setImageBitmap(bitmap);
+                                    return null;
+                                });
+                            }
+                        });
+                    }
+                    return null;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Method to generate dummy data for RecyclerView
@@ -58,4 +113,24 @@ public class PersonalHomepageActivity extends AppCompatActivity {
         // Add dummy data here
         return dongTaiContents;
     }
+
+    // 对HashMap的使用参考UserInformationActivity.java
+    private HashMap<String, String> getInfo() {
+        HashMap<String, String> userInfo = new HashMap<>();
+        userInfo.put("username", "John Doe");
+        userInfo.put("intro", "Hello, I'm John.");
+        userInfo.put("following", "关注：1");
+        userInfo.put("follower", "粉丝：1");
+        return userInfo;
+    }
+    private HashMap<String, String> getAllInputValues() {
+        HashMap<String, String> inputValues = new HashMap<>();
+        inputValues.put("username", usernameTextView.getText().toString());
+        inputValues.put("description", descriptionTextView.getText().toString());
+        inputValues.put("following", followingTextView.getText().toString());
+        inputValues.put("follower", followerTextView.getText().toString());
+        inputValues.put("uid", "");
+        return inputValues;
+    }
+
 }
