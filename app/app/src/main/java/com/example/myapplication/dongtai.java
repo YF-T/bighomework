c3c4db1c3afae09e2e6f7a6840d29bcbd944de2d
SA <870829602@qq.com>
2023-06-06 19:17:00 +0800

    加入content分享

--- app/app/src/main/java/com/example/myapplication/dongtai.java
+++ app/app/src/main/java/com/example/myapplication/dongtai.java
@@ -265,8 +265,8 @@ public class dongtai extends AppCompatActivity {
         Intent shareIntent = new Intent();
 
         shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "SHARE");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, content.getText());
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, this.title.getText().toString());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, content.getText().toString());
 
         shareIntent.setType("text/plain");
         startActivity(Intent.createChooser(shareIntent,title));
